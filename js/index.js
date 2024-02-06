4773c78fcead942c4f813a14920765af42a1e236
Andy <obrezkov.andy@gmail.com>
2024-02-06 22:56:43 +0500

    Add offcanvas

---
+++ js/index.js
@@ -0,0 +1,27 @@
+let theoryContent = document.getElementById("theoryContent");
+let theoryContentHeader = document.getElementById("theoryContentHeader");
+let theoryContentBody = document.getElementById("theoryContentBody");
+let theoryContentHeaderBtn = document.getElementById("theoryContentHeaderBtn");
+
+function changeElementClasses() {
+   let windowWidth = window.innerWidth;
+
+   if (windowWidth < 992) {
+      theoryContent.classList.toggle("offcanvas");
+      theoryContent.classList.toggle("offcanvas-end");
+      theoryContent.setAttribute("tabindex", "-1");
+      theoryContent.setAttribute("aria-labelledby", "theoryContent");
+
+      theoryContentHeader.classList.toggle("offcanvas-header");
+
+      theoryContentBody.classList.toggle("offcanvas-body");
+
+      theoryContentHeaderBtn.classList.toggle("invisible");
+   } else {
+      theoryContent.removeAttribute("tabindex");
+      theoryContent.removeAttribute("aria-labelledby");
+   }
+}
+
+changeElementClasses();
+window.addEventListener("resize", changeElementClasses);
