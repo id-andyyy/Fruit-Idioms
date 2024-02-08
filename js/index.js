203f62c1ea154222ae336c24801ea58d9386fc41
Andy <obrezkov.andy@gmail.com>
2024-02-08 18:59:22 +0500

    Add form verification and display of correct answers

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-let theoryContent = document.getElementById("theoryContent");
-let theoryContentHeader = document.getElementById("theoryContentHeader");
-let theoryContentBody = document.getElementById("theoryContentBody");
-let theoryContentHeaderBtn = document.getElementById("theoryContentHeaderBtn");
+let theoryContentNode = document.getElementById("theoryContent");
+let theoryContentHeaderNode = document.getElementById("theoryContentHeader");
+let theoryContentBodyNode = document.getElementById("theoryContentBody");
+let theoryContentHeaderBtnNode = document.getElementById("theoryContentHeaderBtn");
 
 function changeElementClasses() {
    let windowWidth = window.innerWidth;
 
    if (windowWidth < 992) {
-      theoryContent.classList.toggle("offcanvas");
-      theoryContent.classList.toggle("offcanvas-end");
-      theoryContent.setAttribute("tabindex", "-1");
-      theoryContent.setAttribute("aria-labelledby", "theoryContent");
+      theoryContentNode.classList.toggle("offcanvas");
+      theoryContentNode.classList.toggle("offcanvas-end");
+      theoryContentNode.setAttribute("tabindex", "-1");
+      theoryContentNode.setAttribute("aria-labelledby", "theoryContent");
 
-      theoryContentHeader.classList.toggle("offcanvas-header");
+      theoryContentHeaderNode.classList.toggle("offcanvas-header");
 
-      theoryContentBody.classList.toggle("offcanvas-body");
+      theoryContentBodyNode.classList.toggle("offcanvas-body");
 
-      theoryContentHeaderBtn.classList.toggle("invisible");
+      theoryContentHeaderBtnNode.classList.toggle("invisible");
    } else {
-      theoryContent.removeAttribute("tabindex");
-      theoryContent.removeAttribute("aria-labelledby");
+      theoryContentNode.removeAttribute("tabindex");
+      theoryContentNode.removeAttribute("aria-labelledby");
    }
 }
 
@@ -27,12 +27,33 @@ changeElementClasses();
 window.addEventListener("resize", changeElementClasses);
 
 function serializeForm(formNode) {
-   return new FormData(formNode)
+   return new FormData(formNode);
 }
 
 function handleFormSubmit(event) {
    event.preventDefault();
-   console.log(Array.from(serializeForm(tasksForm).entries()))
+
+   let data = Array.from(serializeForm(tasksForm).entries());
+
+   let points = 0;
+   for (let task in data) {
+      let taskNum = data[task][0].substring(4);
+      let groupNode = document.getElementById(`group${taskNum}`);
+      let inputNode = document.getElementById(data[task][0]);
+      let feedbackNode = document.getElementById(`feedback${taskNum}`);
+
+      feedbackNode.classList.remove("invisible", "position-absolute");
+      if (data[task][1].toLowerCase().replace(".", "") == feedbackNode.textContent.toLowerCase()) {
+         points++;
+         groupNode.classList.add("has-success");
+         inputNode.classList.add("is-valid");
+         feedbackNode.classList.add("valid-feedback");
+      } else {
+         groupNode.classList.add("has-danger");
+         inputNode.classList.add("is-invalid");
+         feedbackNode.classList.add("invalid-feedback");
+      }
+   }
 }
 
 const tasksForm = document.getElementById("tasks");
